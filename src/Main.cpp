a5c99785a5f3930a4f1862e5204d7d00c9c2acd9
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-12-27 18:51:05 +0100

    build: import all dependencies of project

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,8 +4,12 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
 #include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
 int main() {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
     SDL_Init(SDL_INIT_EVERYTHING);
     std::cout << "Hello, world!" << std::endl;
     return 0;
