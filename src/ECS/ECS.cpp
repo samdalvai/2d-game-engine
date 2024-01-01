f7b3df22436f22511dc3c65e9f4f9b6694b593f8
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-01 18:46:36 +0100

    Add implementations of add entity and remove entity

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,15 +1,29 @@
 #include "ECS.h"
+#include "../Logger/Logger.h"
 
 int Entity::GetId() const {
     return id;
 }
 
 void System::AddEntityToSystem(Entity entity) {
-
+    entities.push_back(entity);
 }
 
 void System::RemoveEntityFromSystem(Entity entity) {
+    int entityPosition = -1;
 
+    for (int i = 0; i < entities.size(); i++) {
+        if (entities[i].GetId() == entity.GetId()) {
+            entityPosition = i;
+            break;
+        }
+    }
+
+    if (entityPosition != -1) {
+        entities.erase(entities.begin() + entityPosition);
+    } else {
+        Logger::Err("Failed to delete entity with id: " + entity.GetId());
+    }
 }
 
 std::vector<Entity> System::GetSystemEntities() const {
