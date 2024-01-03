69de7159db73305b5f0b0fc544ad157d6d7851f4
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-03 16:34:23 +0100

    Add default constructor for passing an entity

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -35,8 +35,10 @@ class Entity {
 
     public:
         Entity(int id): id(id) {};
+        Entity(const Entity& entity) = default;
         int GetId() const;
         
+        Entity& operator = (const Entity& other) = default;
         bool operator == (const Entity& other) const { return id == other.GetId(); }
         bool operator != (const Entity& other) const { return id != other.GetId(); }
 };
