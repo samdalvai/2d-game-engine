5b2a047262fd0e9f4c9822f33da8a8fb74c74b0d
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-05 10:57:08 +0100

    Add start time to animation component

--- src/Components/AnimationComponent.h
+++ src/Components/AnimationComponent.h
@@ -9,12 +9,14 @@ struct AnimationComponent {
     int currentFrame;
     int frameRateSpeed;
     bool shouldLoop;
+    int startTime;
 
     AnimationComponent(int numFrames = 1, int currentFrame = 1, int frameRateSpeed = 1, bool shouldLoop = true) {
         this->numFrames = numFrames;
         this->currentFrame = currentFrame;
         this->frameRateSpeed = frameRateSpeed;
         this->shouldLoop = shouldLoop;
+        this->startTime = SDL_GetTicks();
     }
 };
 
