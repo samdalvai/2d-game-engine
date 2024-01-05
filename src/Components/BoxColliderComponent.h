938e1f634cf27ec7af3b4c1064be3b27e1d3d2c1
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-05 11:51:17 +0100

    Add main parameters to box collider component

--- src/Components/BoxColliderComponent.h
+++ src/Components/BoxColliderComponent.h
@@ -1,13 +1,19 @@
 #ifndef BOXCOLLIDERCOMPONENT_H
 #define BOXCOLLIDERCOMPONENT_H
 
-#include <SDL2/SDL.h>
+//#include <SDL2/SDL.h>
+#include <glm/glm.hpp>
 
 struct BoxColliderComponent {
+    int width;
+    int height;
+    glm::vec2 offset;
 
-    BoxColliderComponent() {
-        
-    }
+    BoxColliderComponent(int width = 0, int height = 0, glm::vec2 offset = glm::vec2(0)) {
+        this->width = width;
+        this->height = height;
+        this->offset = offset;
+    }   
 };
 
 #endif
