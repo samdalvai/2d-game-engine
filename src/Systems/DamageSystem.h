2312e020995b8bca895946689bc906d67186de8d
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-10 17:37:59 +0100

    Implement rudimentary damage system

--- src/Systems/DamageSystem.h
+++ src/Systems/DamageSystem.h
@@ -5,6 +5,7 @@
 #include "../ECS/ECS.h"
 #include "../EventBus/EventBus.h"
 #include "../Components/BoxColliderComponent.h"
+#include "../Components/HealthComponent.h"
 #include "../Event/CollisionEvent.h"
 
 class DamageSystem: public System {
@@ -19,6 +20,30 @@ class DamageSystem: public System {
 
         void OnCollision(CollisionEvent& event) {
             Logger::Log("Collision occurred between entities: " + std::to_string(event.a.GetId()) + " and " + std::to_string(event.b.GetId()));
+
+            Entity& a = event.a;
+            Entity& b = event.b;
+
+            if (a.HasComponent<HealthComponent>()) {
+                auto& health = a.GetComponent<HealthComponent>();
+                health.healthPercentage -= 25;
+                Logger::Log("Entity: " + std::to_string(event.a.GetId()) + " has now " + std::to_string(health.healthPercentage) + " of health");
+
+                if (health.healthPercentage <= 0) {
+                    a.Kill();
+                }
+            }
+
+            if (b.HasComponent<HealthComponent>()) {
+                auto& health = b.GetComponent<HealthComponent>();
+                health.healthPercentage -= 25;
+                Logger::Log("Entity: " + std::to_string(event.b.GetId()) + " has now " + std::to_string(health.healthPercentage) + " of health");
+
+                if (health.healthPercentage <= 0) {
+                    b.Kill();
+                }
+            }
+
             //event.a.Kill();
             //event.b.Kill();
         }
