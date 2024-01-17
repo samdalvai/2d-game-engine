65b9fb068b87f2755c335c88d467a3cad45ff075
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-17 20:31:46 +0100

    Add fonts to asset store

--- src/AssetStore/AssetStore.cpp
+++ src/AssetStore/AssetStore.cpp
@@ -16,6 +16,8 @@ void AssetStore::ClearAssets() {
         SDL_DestroyTexture(texture.second);
     }
     textures.clear();
+
+    // TODO: clear all the fonts
 }
 
 void AssetStore::AddTexture(SDL_Renderer* renderer, const std::string& assetId, const std::string& filePath) {
@@ -31,4 +33,12 @@ void AssetStore::AddTexture(SDL_Renderer* renderer, const std::string& assetId, 
 
 SDL_Texture* AssetStore::GetTexture(const std::string& assetId) {
     return textures[assetId];
+}
+
+void AssetStore::AddFont(const std::string& assetId, const std::string& filePath, int fontSize) {
+    fonts.emplace(assetId, TTF_OpenFont(filePath.c_str(), fontSize));
+}
+
+TTF_Font* AssetStore::GetFont(const std::string& assetId) {
+    return fonts[assetId];
 }
--- src/AssetStore/AssetStore.h
+++ src/AssetStore/AssetStore.h
@@ -4,11 +4,12 @@
 #include <map>
 #include <string>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 class AssetStore {
     private:
         std::map<std::string, SDL_Texture*> textures;
-        // TODO: create a map for fonts
+        std::map<std::string, TTF_Font*> fonts;
         // TODO: create a map for audio
 
     public:
@@ -18,6 +19,8 @@ class AssetStore {
         void ClearAssets();
         void AddTexture(SDL_Renderer* renderer, const std::string& assetId, const std::string& filePath);
         SDL_Texture* GetTexture(const std::string& assetId);
+        void AddFont(const std::string& assetId, const std::string& filePath, int fontSize);
+        TTF_Font* GetFont(const std::string& assetId);
 };
 
 #endif
