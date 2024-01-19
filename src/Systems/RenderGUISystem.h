698e79d7681aa073793ccae82bf73fad466a2daf
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-19 19:56:44 +0100

    Add functionality to create enemy with imgui

--- src/Systems/RenderGUISystem.h
+++ src/Systems/RenderGUISystem.h
@@ -15,10 +15,25 @@ class RenderGUISystem: public System {
             ImGui::NewFrame();
             
             if (ImGui::Begin("Spawn enemies")) {
-                ImGui::Text("Here you can spawn enemies");
+                ImGui::Text("Spawn new enemy");
+                static int enemyPositionX = 100;
+                ImGui::InputInt("Enemey position x", &enemyPositionX);
+
+                static int enemyPositionY = 100;
+                ImGui::InputInt("Enemy position y", &enemyPositionY);
+
+                if (ImGui::Button("Click to create enemy")) {
+                    Entity enemy = registry->CreateEntity();
+                    enemy.Group("enemies");
+                    enemy.AddComponent<TransformComponent>(glm::vec2(enemyPositionX, enemyPositionY), glm::vec2(1.0, 1.0), 0.0);
+                    enemy.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
+                    enemy.AddComponent<SpriteComponent>("truck-image", 32, 32, 2);
+                    enemy.AddComponent<BoxColliderComponent>(32, 32);
+                    enemy.AddComponent<ProjectileEmitterComponent>(glm::vec2(0.0, 100.0), 2000, 5000, 25, false);
+                    enemy.AddComponent<HealthComponent>(100);
+                }
             }
             ImGui::End();
-
 
             ImGui::Render();
             ImGuiSDL::Render(ImGui::GetDrawData());
