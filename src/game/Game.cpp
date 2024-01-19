cdda6ef42f484d2eae7f42f4918af5b04ae70047
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-19 18:59:20 +0100

    Add mouse events for imgui

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -109,6 +109,16 @@ void Game::Initialize() {
 void Game::ProcessInput() {
     SDL_Event sdlEvent;
     while (SDL_PollEvent(&sdlEvent)) {
+        if (isDebug) {
+            ImGui_ImplSDL2_ProcessEvent(&sdlEvent);
+            ImGuiIO& io = ImGui::GetIO();
+            int mouseX, mouseY;
+            const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
+            io.MousePos = ImVec2(mouseX, mouseY);
+            io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+            io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
+        }
+
         switch (sdlEvent.type) {
             case SDL_QUIT:
                 isRunning = false;
