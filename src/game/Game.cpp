70fbaba304de00357be9ad39cbfb6ef7d825001a
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-19 20:06:35 +0100

    Make chopper start at center of windorw

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -199,7 +199,7 @@ void Game::LoadLevel(int level) {
     // Create an entity
     Entity chopper = registry->CreateEntity();
     chopper.Tag("player");
-    chopper.AddComponent<TransformComponent>(glm::vec2(100.0, 100.0), glm::vec2(1.0, 1.0), 0.0);
+    chopper.AddComponent<TransformComponent>(glm::vec2(windowWidth / 2, windowHeight / 2), glm::vec2(1.0, 1.0), 0.0);
     chopper.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
     chopper.AddComponent<SpriteComponent>("chopper-image", 32, 32, 1);
     chopper.AddComponent<AnimationComponent>(2, 15, true);
