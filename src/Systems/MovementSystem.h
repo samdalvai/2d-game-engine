4f1d872ff68eaff50685cac1d7fdaaf774c45ae4
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-21 14:20:06 +0100

    Kill entities outside of map

--- src/Systems/MovementSystem.h
+++ src/Systems/MovementSystem.h
@@ -5,24 +5,33 @@
 #include "../Components/TransformComponent.h"
 #include "../Components/RigidBodyComponent.h"
 
-class MovementSystem: public System {
-    public:
-        MovementSystem() {
-            RequireComponent<TransformComponent>();
-            RequireComponent<RigidBodyComponent>();
-        }
+class MovementSystem : public System {
+public:
+    MovementSystem() {
+        RequireComponent<TransformComponent>();
+        RequireComponent<RigidBodyComponent>();
+    }
 
-        void Update(double deltaTime) {
-            // Loop all entities that the system is interested in
-            for (auto entity: GetSystemEntities()) {
-                // Update entity position based on its velocity
-                auto& transform = entity.GetComponent<TransformComponent>();
-                const auto rigidbody = entity.GetComponent<RigidBodyComponent>();
+    void Update(double deltaTime) {
+        // Loop all entities that the system is interested in
+        for (auto entity : GetSystemEntities()) {
+            // Update entity position based on its velocity
+            auto &transform = entity.GetComponent<TransformComponent>();
+            const auto rigidbody = entity.GetComponent<RigidBodyComponent>();
 
-                transform.position.x += rigidbody.velocity.x * deltaTime; 
-                transform.position.y += rigidbody.velocity.y * deltaTime; 
+            transform.position.x += rigidbody.velocity.x * deltaTime;
+            transform.position.y += rigidbody.velocity.y * deltaTime;
+
+            bool isEntityOutsideMap = transform.position.x < 0 ||
+                                      transform.position.x > Game::mapWidth ||
+                                      transform.position.y < 0 ||
+                                      transform.position.y > Game::mapHeight;
+
+            if (isEntityOutsideMap && !entity.HasTag("player")) {
+                entity.Kill();
             }
         }
+    }
 };
 
 #endif
