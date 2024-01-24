aacc9e839872f57c6b6f3ef2992ae41f713efe1f
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-24 19:31:37 +0100

    Add test function for lua script

---
+++ assets/scripts/myscript.lua
@@ -0,0 +1,8 @@
+-- This is a Lua variable
+some_variable = 7 * 6
+user_name = "Samuel"
+
+if some_variable == 42 then
+    print("The variable is: "..some_variable)
+    print("Name is: "..user_name)
+end
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,24 @@
 #include "./Game/Game.h"
+#include <sol/sol.hpp>
+
+void TestLua() {
+    sol::state lua;
+
+    lua.script_file("assets/scripts/myscript.lua");
+
+    int someVariable = lua["some_variable"];
+
+    std::cout << "This is the value" << std::to_string(someVariable) << std::endl;
+}
 
 int main(int argc, char* argv[]) {
-    Game game;
+    // Game game;
 
-    game.Initialize();
-    game.Run();
-    game.Destroy();
+    // game.Initialize();
+    // game.Run();
+    // game.Destroy();
+
+    TestLua();
 
     return 0;
 }
