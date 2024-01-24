01b535cf019b9f69e3a465f975c46aa0fdf5730f
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-24 20:17:46 +0100

    Run lua function from x++ code

--- src/Main.cpp
+++ src/Main.cpp
@@ -20,6 +20,10 @@ void TestLua() {
     int height = config["resolution"]["height"];
 
     std::cout << "Height: " << std::to_string(width) << " Width: " << std::to_string(height) << std::endl;
+
+    sol::function functionFactorial = lua["factorial_recursive"];
+    int result = functionFactorial(5);
+    std::cout << "Factorial in C++: " << std::to_string(result) << std::endl;
 }
 
 int main(int argc, char* argv[]) {
