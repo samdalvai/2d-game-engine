a2b02d515990196624702693aa000e27fe960fbb
Samuel Dalvai <samuel.dalvai@ewo.com>
2024-01-24 19:43:28 +0100

    Add lua table example

--- assets/scripts/myscript.lua
+++ assets/scripts/myscript.lua
@@ -8,3 +8,13 @@ print("This is the value in Lua: "..some_variable)
 if some_variable == 42 then
     print("The variable is 42")
 end
+
+-- Lua table with values for initialization of Game
+config = {
+    title = "My table",
+    fullScreen = true,
+    resolution = {
+        width = 800,
+        height = 600
+    }
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -10,6 +10,10 @@ void TestLua() {
     int someVariable = lua["some_variable"];
 
     std::cout << "This is the value in C++: " << std::to_string(someVariable) << std::endl;
+
+    bool fullScreen = lua["config"]["fullScreen"];
+
+    std::cout << "Fullscreen? " << std::to_string(fullScreen) << std::endl;
 }
 
 int main(int argc, char* argv[]) {
